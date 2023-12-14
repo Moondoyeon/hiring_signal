c230404590961858ad783a5a3f4e96a388236791
Moondoyeon <mmmdo21@gmail.com>
2023-12-14 12:48:51 +0900

    feat: 영상섹션 제외한 섹션에서 헤더컬러 검정

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,55 +1,76 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import { Link } from "react-scroll";
+import { useCallback, useEffect, useRef, useState } from "react";
 import SrollMove from "./SrollMove";
 
 export default function Header() {
   const [visible, setVisible] = useState(true);
+  const [currentScrollY, setCurrentScrollY] = useState(0);
   const beforeScrollY = useRef(0);
 
-  const handleScroll2 = () => {
-    const currentScrollY = window.scrollY;
+  const handleHeaderVisible = useCallback(() => {
     if (beforeScrollY.current < currentScrollY) setVisible(false);
     else setVisible(true);
     beforeScrollY.current = currentScrollY;
-  };
+  }, [currentScrollY]);
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll2);
+    window.addEventListener("scroll", handleHeaderVisible);
+    return () => {
+      window.addEventListener("scroll", handleHeaderVisible);
+    };
+  }, [handleHeaderVisible]);
+
+  const headColor = useRef("white");
+  headColor.current = currentScrollY < 1000 ? "white" : "black";
+  const handleCurrentScrollY = useCallback(() => {
+    setCurrentScrollY(window.scrollY);
   }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleCurrentScrollY);
+    return () => {
+      window.addEventListener("scroll", handleCurrentScrollY);
+    };
+  }, [handleCurrentScrollY]);
 
   if (visible)
     return (
-      <header className="fixed w-full z-50 bg-transparent flex justify-between items-center px-8 py-4 border-b border-white">
+      <header
+        className={`fixed w-full z-50 bg-transparent flex justify-between items-center px-8 py-4 border-b border-${headColor.current}`}
+      >
         <a href="/">
-          <h1 className="w-fit border-y border-white text-3xl font-semibold text-white">
+          <h1
+            className={`w-fit border-y border-${headColor.current} text-3xl font-semibold text-${headColor.current}`}
+          >
             채용시그널
           </h1>
         </a>
         {/* onSetActive 속성에 setState 넘겨서 active state를 boolean 값으로 활성화상태 css 자유롭게줄수있지않을까?  */}
-        <ul className="relative flex justify-between text-2xl font-semibold text-white">
+        <ul
+          className={`relative flex justify-between text-2xl font-semibold text-${headColor.current}`}
+        >
           <SrollMove
             to="/main/#work"
-            activeStyle={{ borderBottom: "2px black solid" }}
+            activeStyle={{ borderBottom: `2px ${headColor.current} solid` }}
           >
             <li className="ml-6 cursor-pointer">WORK</li>
           </SrollMove>
           <SrollMove
             to="/main/#project"
-            activeStyle={{ borderBottom: "2px black solid" }}
+            activeStyle={{ borderBottom: `2px ${headColor.current} solid` }}
           >
             <li className="ml-6 cursor-pointer">PROJECT</li>
           </SrollMove>
           <SrollMove
             to="/main/#study"
-            activeStyle={{ borderBottom: "2px black solid" }}
+            activeStyle={{ borderBottom: `2px ${headColor.current} solid` }}
           >
             <li className="ml-6 cursor-pointer">STUDY</li>
           </SrollMove>
           <SrollMove
             to="/main/#signal"
-            activeStyle={{ borderBottom: "2px black solid" }}
+            activeStyle={{ borderBottom: `2px ${headColor.current} solid` }}
           >
             <li className="ml-6 cursor-pointer">SIGNAL</li>
           </SrollMove>
