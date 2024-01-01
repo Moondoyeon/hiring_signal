fb65137ad792caf2da580ebe502865d26254b513
Moondoyeon <mmmdo21@gmail.com>
2024-01-01 22:44:25 +0900

    move: font 위치변경

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -15,18 +15,18 @@ import localFont from 'next/font/local';
 
 export const notoSansKr = localFont({
   src: [
-    { path: '../../public/fonts/NotoSansKR-light.ttf', weight: '300' },
-    { path: '../../public/fonts/NotoSansKR-Regular.ttf', weight: '400' },
-    { path: '../../public/fonts/NotoSansKR-Medium.ttf', weight: '500' },
-    { path: '../../public/fonts/NotoSansKR-SemiBold.ttf', weight: '600' },
-    { path: '../../public/fonts/NotoSansKR-Bold.ttf', weight: '700' },
+    { path: '../fonts/NotoSansKR-light.ttf', weight: '300' },
+    { path: '../fonts/NotoSansKR-Regular.ttf', weight: '400' },
+    { path: '../fonts/NotoSansKR-Medium.ttf', weight: '500' },
+    { path: '../fonts/NotoSansKR-SemiBold.ttf', weight: '600' },
+    { path: '../fonts/NotoSansKR-Bold.ttf', weight: '700' },
   ],
   display: 'swap',
 });
 export const gowunBatang = localFont({
   src: [
-    { path: '../../public/fonts/GowunBatang-Regular.ttf', weight: '400' },
-    { path: '../../public/fonts/GowunBatang-Bold.ttf', weight: '700' },
+    { path: '../fonts/GowunBatang-Regular.ttf', weight: '400' },
+    { path: '../fonts/GowunBatang-Bold.ttf', weight: '700' },
   ],
   display: 'swap',
 });
