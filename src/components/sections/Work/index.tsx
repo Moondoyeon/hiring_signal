1a4515aa50559ce4d7b6d510bec3a458f4858946
Moondoyeon <mmmdo21@gmail.com>
2024-01-02 13:27:38 +0900

    Revert "style: css 수정"
    This reverts commit 01b1d8b69cdbc98326a2c969735a4d974caa55de.

--- src/components/sections/Work/index.tsx
+++ src/components/sections/Work/index.tsx
@@ -30,8 +30,8 @@ export default async function WorkSection() {
   ];
 
   return (
-    <SectionContainer observeSection="work-section" style="pt-36 mobile:pt-20">
-      <h2 className="pb-12 text-4xl font-semibold mobile:text-2xl mobile:pb-8">WORK</h2>
+    <SectionContainer observeSection="work-section" style="pt-40 mobile:pt-20">
+      <h2 className="pb-12 text-4xl font-gown font-semibold mobile:text-2xl mobile:pb-8">WORK</h2>
 
       <Content works={fake} />
     </SectionContainer>
