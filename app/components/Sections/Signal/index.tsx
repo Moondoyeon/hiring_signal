741805751436bd704feced1addc8bf4c940c541e
Moondoyeon <mmmdo21@gmail.com>
2024-01-25 12:22:21 +0900

    style: 문구수정

--- app/components/Sections/Signal/index.tsx
+++ app/components/Sections/Signal/index.tsx
@@ -4,7 +4,8 @@ import SignalForm from '@components/Sections/Signal/SignalForm';
 export default function SignalSection() {
   return (
     <SectionContainer observeSection="signal-section" style="pt-40 pb-52 mobile:pt-16 mobile:pb-32">
-      <h2 className="pb-12 text-4xl font-semibold mobile:text-3xl mobile:pb-8">SIGNAL</h2>
+      <h2 className="text-4xl font-semibold mobile:text-3xl ">SIGNAL</h2>
+      <p className="pb-12 mobile:pb-8">폼작성 및 제출시 제게 이메일이 발송됩니다 :D</p>
       <SignalForm />
     </SectionContainer>
   );
